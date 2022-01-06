b2a263296a83aa6f5724613c8cb659bf32e61f1f
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-06 22:26:41 +0000

    Change getAllProperties to make a SQL request to select data from database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,13 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /// Users
 
@@ -38,7 +46,7 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
+const addUser = function(user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
@@ -66,12 +74,16 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool.
+    query(`
+    SELECT * 
+    FROM properties 
+    LIMIT $1`, [limit])
+    .then((result) => result.rows)
+    .catch((error) => {
+      console.log('error getting all properties:', error.message)
+    });
 }
 exports.getAllProperties = getAllProperties;
 
